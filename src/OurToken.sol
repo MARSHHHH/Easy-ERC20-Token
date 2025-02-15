6a1f0ed420620cfe6470e41d04d86679478cf1c3
marsh <2636024301@qq.com>
2025-02-15 15:13:44 +0000

    a test

---
+++ src/OurToken.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+//testing
+pragma solidity ^0.8.18;
+/* Creating ERC20 token using OpenZeppelin Library */
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract OurToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("OurToken", "OT") {
+        _mint(msg.sender, initialSupply);
+    }
+}
