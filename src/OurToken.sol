903e8d5461bd54859c540a43f5810033fb048eb3
marsh <2636024301@qq.com>
2025-02-16 19:08:38 +0000

    Updating test.yml

--- src/OurToken.sol
+++ src/OurToken.sol
@@ -2,6 +2,7 @@
 //testing
 pragma solidity ^0.8.18;
 /* Creating ERC20 token using OpenZeppelin Library */
+
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract OurToken is ERC20 {
